cc70c580fb8c96bed0da3003198436f203570768
Nicolas Bermudez <bermnicolas@gmail.com>
2019-05-03 01:47:09 +0000

    Automatically backed up by Learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -49,6 +49,5 @@ export class RestaurantInput extends Component {
   }
 };
 
-
 //connect this component by wrapping RestaurantInput below
 export default connect(null, { addRestaurant })(RestaurantInput);
